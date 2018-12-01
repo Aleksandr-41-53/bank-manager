107972c54b091834a5f75d977dbf98f1b50c5fb4
Александр <darkginesys@gmail.com>
2018-12-01 14:13:44 +0300

    init

---
+++ src/main/java/com/bank/bankmanager/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.bank.bankmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("user", "Folk-Lore");
+        return "index";
+    }
+
+}
---
+++ src/test/java/com/bank/bankmanager/BankManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bank.bankmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
